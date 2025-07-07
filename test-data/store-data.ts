167858546a3e654f7223797ecae12a725dc45f4b
Nadezhda0490 <nadjastrelchenok@gmail.com>
2025-07-08 01:33:53 +0300

    added  API tests for user, pet and store endpoints

---
+++ test-data/store-data.ts
@@ -0,0 +1,11 @@
+export const petStatus = ["available", "pending", "sold"];
+
+export const testOrder = {
+  id: 234,
+  petId: 11112,
+  quantity: 1,
+  status: "approved",
+  complete: true,
+};
+
+export const invalidOrderId = -1;
---
+++ test-data/user-data.ts
@@ -0,0 +1,23 @@
+export const testUser = {
+  id: 2224,
+  username: "nadjat",
+  firstName: "Nadja",
+  lastName: "Test",
+  email: "nadjat@gmail.com",
+  password: "password123",
+  phone: "1234567890",
+  userStatus: 1,
+};
+
+export const invalidUsername = "nadja";
+
+export const updatedUser = {
+  id: 2224,
+  username: "nadja_upd",
+  firstName: "Nadja",
+  lastName: "Update",
+  email: "nadjaupd@gmail.com",
+  password: "password123upd",
+  phone: "1234567890",
+  userStatus: 1,
+};
